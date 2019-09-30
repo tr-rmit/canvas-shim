0a8a3b4705ef3583b01b35cb6ee551ac6e4126d5
Trevor Reynolds <trevor.reynolds@rmit.edu.au>
2019-09-30 15:27:06 +1000

    Unassigned students top re-tweak

--- js.js
+++ js.js
@@ -36,6 +36,15 @@ window.onscroll = function() {
 /* Makes the course navigation and unenrolled students "sticky"
    Magic Numbers: 25px and -40px (nav) works well now but this may change in the future. */
   var ls = document.getElementById('left-side');
+  
+// wtf? There are two of them?  	
+  var uas = document.getElementsByClassName('unassigned-students')[1]; 	
+  var minTop = Math.max(-40,(25-window.pageYOffset))+"px";	
+  ls.style.top = minTop;	
+  minTop = Math.max(0,(window.pageYOffset-250))+"px";	
+  if (uas.style.position != 'relative')	
+    uas.style.position = 'relative';	
+  uas.style.top=minTop;
 
 // Remove brs to increase realestate in groups area
   var tags = document.getElementsByClassName('show-group-full');
